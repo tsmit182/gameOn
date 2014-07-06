709ba682a1c1c58b8326ad67c956f436ef07ce0c
Taylor Smith <tsmit182@gmail.com>
2014-07-06 10:21:35 -0400

    add content to main.js - three object constructors, first iterations of each object

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,36 @@
-console.log('The Iron Yard Rocks');
+
+//room construction:
+var Room = function (options) {
+	var options = options || {};
+	this.name = options.name;
+	this.description = options.description;
+	this.exits = [];
+};
+
+//creature construction:
+var Creature = function (options) {
+	var options = options || {};
+	this.name = options.name;
+	this.description = options.description;
+	this.onTouch = options.onTouch;
+	this.onAttack = options.onAttack;
+};
+
+var roomOne = new Room ({
+	name: 'betweenlands',
+	description: 'What you feel to be the betweenlands are a rather blank area within which to contemplate what you have or have not done.',
+	// exits: [south]
+});
+
+var shadowEntity = new Creature ({
+	name: "Shadow Entity",
+	description: "You think you saw it out of your peripheral vision but when you turn to look the shadowy figure is gone.",
+	onTouch: "You grasp out into the air around you- to your side, behind you- but it feels like nothing is there.",
+	onAttack: "Fighting shadows, are we?",
+});
+
+//print:
+// var print= function (options)
+
+
+
